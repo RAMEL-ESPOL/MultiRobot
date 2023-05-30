365ba6713de5769062830e6a9ddc6c6ff70786f6
DiegoRonquillo4 <diromano@espol.edu.ec>
2023-05-30 14:14:54 -0500

    Simulation with two turtlebots3

--- ramel/launch/multi_robot_simulation.launch.py
+++ ramel/launch/multi_robot_simulation.launch.py
@@ -1,31 +1,121 @@
-import os
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, Command
+#!/usr/bin/env python3
+#
+# ... (License and author information)
 
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 def generate_launch_description():
-    # Get the path to the turtlebot3_gazebo package
-    turtlebot3_gazebo_path = os.path.join(os.getcwd(), 'install', 'turtlebot3_gazebo','share','turtlebot3_gazebo')
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
-    # Launch turtlebot3_gazebo empty_world.launch.py for the first TurtleBot3
-    turtlebot3_1_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(turtlebot3_gazebo_path, 'launch', 'empty_world.launch.py')),
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+
+    world = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'worlds',
+        'empty_world.world'
     )
 
-     # Launch turtlebot3_gazebo spawn.launch.py for the second TurtleBot3
-    turtlebot3_2_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(turtlebot3_gazebo_path, 'launch', 'spawn_turtlebot3.launch.py')),
-        launch_arguments={
-            'model': 'turtlebot3_burger',
-            'x_pose': '2.0',
-            'y_pose': '0.0',
-            'robot_name': LaunchConfiguration('robot_name')
-        }.items()
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
     )
 
-    return LaunchDescription([
-        turtlebot3_1_launch,
-        DeclareLaunchArgument('robot_name', default_value='tb3_2', description='Name of the second TurtleBot3'),
-    ])
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
+    # Get the model and urdf file
+    model_folder = 'turtlebot3_burger'
+    model_path = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'models',
+        model_folder,
+        'model.sdf'
+    )
+    urdf_file_name = 'turtlebot3_burger.urdf'
+    urdf_path = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'urdf',
+        urdf_file_name
+        )
+    start_gazebo_ros_spawner_cmd = Node(
+    package='gazebo_ros',
+    executable='spawn_entity.py',
+    arguments=[
+        '-entity', 'tb3',
+        '-file', model_path,
+        '-x', x_pose,
+        '-y', y_pose,
+        '-z', '0.01',
+         '-robot_namespace', 'tb3'  # Set the robot namespace
+    ],
+    output='screen',
+    )
+    start_gazebo_ros_spawner_cmd_2 = Node(
+    package='gazebo_ros',
+    executable='spawn_entity.py',
+    arguments=[
+        '-entity', 'tb3_2',
+        '-file', model_path,
+        '-x', '2.0',
+        '-y', '2.0',
+        '-z', '0.01',
+         '-robot_namespace', 'tb3_2'  # Set the robot namespace
+    ],
+    output='screen',
+    )
+    with open(urdf_path, 'r') as infp:
+        robot_desc = infp.read()
+
+    robot_state_publisher_cmd_1 = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        namespace='tb3',  # Set the namespace for the first robot state publisher
+        output='screen',
+        parameters=[{
+            'use_sim_time': use_sim_time,
+            'robot_description': robot_desc
+        }],
+    )
+    robot_state_publisher_cmd_2 = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        namespace='tb3_2',  # Set the namespace for the second robot state publisher
+        output='screen',
+        parameters=[{
+            'use_sim_time': use_sim_time,
+            'robot_description': robot_desc,
+        }],
+    )
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+    ld = LaunchDescription()
+
+    # Add the commands to the launch description
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(robot_state_publisher_cmd_1)
+    ld.add_action(robot_state_publisher_cmd_2)
+    ld.add_action(start_gazebo_ros_spawner_cmd)
+    ld.add_action(start_gazebo_ros_spawner_cmd_2)
+
+    return ld
