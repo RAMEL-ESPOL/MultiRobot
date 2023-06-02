7e4e98025d05c6d1994cfffa2753531f6d862e98
antepigu <antepigu@espol.edu.ec>
2023-06-02 09:39:33 -0500

    Aruco node added

--- ramel/launch/ramel.launch.py
+++ ramel/launch/ramel.launch.py
@@ -5,14 +5,20 @@ from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    # Launch realsense2_camera node
-    realsense_camera_node = ExecuteProcess(
+    # Launch aruco_ros2 node
+    """aruco_marker_publisher_node = ExecuteProcess(
         cmd=['ros2', 'launch', 'aruco_ros2', 'marker_publisher.launch.xml'],
         output='screen'
-    )
+    )"""
 
     # Launch aruco_ros2 node
     aruco_marker_publisher_node = ExecuteProcess(
+        cmd=['ros2', 'run', 'ros2_aruco', 'aruco_node'],
+        output='screen'
+    )
+
+    # Launch realsense2_camera node
+    realsense_camera_node = ExecuteProcess(
         cmd=['ros2', 'launch', 'realsense2_camera', 'rs_launch.py', 'rgb_camera.profile:=1920x1080x30'],
         output='screen'
     )
