4e1c3f133794b98f038b048c87b6d5183aff50a8
antepigu <antepigu@espol.edu.ec>
2023-06-27 14:59:12 -0500

    Rviz use uf namespace for cameras

--- realsense-ros/realsense2_description/launch/gazebo.launch.py
+++ realsense-ros/realsense2_description/launch/gazebo.launch.py
@@ -1,7 +1,6 @@
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.actions import SetEnvironmentVariable
+from launch.actions import OpaqueFunction, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, Command, EnvironmentVariable
 from launch_ros.actions import Node
@@ -9,11 +8,40 @@ from ament_index_python.packages import get_package_share_directory
 from launch.actions import ExecuteProcess
 from launch_utils import to_urdf
 from launch.conditions import IfCondition
+from pathlib import Path
+import xacro
+
+def evaluate_xacro(context, *args, **kwargs):
+    name = LaunchConfiguration('name').perform(context)
+    topics_ns = LaunchConfiguration('topics_ns').perform(context)
+    add_plug = LaunchConfiguration('add_plug').perform(context)
+    publish_pointcloud = LaunchConfiguration('publish_pointcloud').perform(context)
+    use_nominal_extrinsics = LaunchConfiguration('use_nominal_extrinsics').perform(context)
+    use_mesh = LaunchConfiguration('use_mesh').perform(context)
+    model = LaunchConfiguration('model').perform(context)
+
+    models = os.path.join(get_package_share_directory('realsense2_description'))
+
+    robot_desc = xacro.process_file(model, mappings={'name': name,
+                                                    'topics_ns': topics_ns,
+                                                    'add_plug': add_plug,
+                                                    'publish_pointcloud': publish_pointcloud,
+                                                    'use_nominal_extrinsics': use_nominal_extrinsics,
+                                                    'use_mesh': use_mesh}).toxml()
+    
+    xml = robot_desc.replace('package://realsense2_description', models)
+
+    swpan_args = '{name: \"cam_l515\", xml: \"'  +  xml + '\" }'
+    urdf_spawner = ExecuteProcess(
+            cmd=['ros2', 'service', 'call', '/spawn_entity', 'gazebo_msgs/SpawnEntity', swpan_args],
+            output='screen'
+    )
+
+    return [urdf_spawner]
 
 def generate_launch_description():
     # Arguments
     xacro_path = os.path.join(get_package_share_directory('realsense2_description'), 'urdf', 'cam_l515.urdf.xacro')
-    urdf = to_urdf(xacro_path, {'use_nominal_extrinsics': 'true', 'add_plug': 'true'})
     paused = LaunchConfiguration('paused', default='false')
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     gui = LaunchConfiguration('gui', default='true')
@@ -25,10 +53,17 @@ def generate_launch_description():
     add_plug = LaunchConfiguration('add_plug', default='false')
     publish_pointcloud = LaunchConfiguration('publish_pointcloud', default='$(arg publish_pointcloud)')
     
-    
+    #name_var = str(LaunchConfiguration('name'))
+    #name_str = LaunchContext.perform_substitution(name)
+    #topic_var = str(LaunchConfiguration('topics_ns'))
+    #topic_str = LaunchContext.perform_substitution(LaunchConfiguration(topics_ns))
+
+    urdf = to_urdf(xacro_path, {'name':str(name), 'topics_ns':str(topics_ns), 'use_nominal_extrinsics': 'true', 'add_plug': 'true'})
+
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     robot_name = 'cam_l515'
-    world_file_name = 'floating_marker.world' #'empty.world'
+    #world_file_name = 'floating_marker.world' #'empty.world'
+    world_file_name = 'empty.world'
     
     world = os.path.join(get_package_share_directory('realsense2_description'), 'worlds', world_file_name)
     models = os.path.join(get_package_share_directory('realsense2_description'))
@@ -39,7 +74,7 @@ def generate_launch_description():
     
     xml = xml.replace('package://realsense2_description', models)
     
-    swpan_args = '{name: \"cam_l515\", xml: \"'  +  xml + '\" }'
+    #swpan_args = '{name: \"cam_l515\", xml: \"'  +  xml + '\" }'
     
     # Set GAZEBO_MODEL_URI to empty string to prevent Gazebo from downloading models
     gz_model_uri = SetEnvironmentVariable(name='GAZEBO_MODEL_URI', value=[''])
@@ -97,10 +132,10 @@ def generate_launch_description():
     )"""
     
     # URDF Spawner Node
-    urdf_spawner = ExecuteProcess(
+    """urdf_spawner = ExecuteProcess(
             cmd=['ros2', 'service', 'call', '/spawn_entity', 'gazebo_msgs/SpawnEntity', swpan_args],
             output='screen'
-    )
+    )"""
     """
     urdf_spawner = Node(
         package='gazebo_ros',
@@ -121,6 +156,6 @@ def generate_launch_description():
         gzclient,
         #gazebo_cmd,
     	#empty_world_launch,
-    	urdf_spawner
+    	#urdf_spawner
+        OpaqueFunction(function=evaluate_xacro)
     ])
-
--- realsense-ros/realsense2_description/launch/view_l515_model_rviz_gazebo.launch.py
+++ realsense-ros/realsense2_description/launch/view_l515_model_rviz_gazebo.launch.py
@@ -1,19 +1,46 @@
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetLaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import OpaqueFunction
 import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from launch_utils import to_urdf
+import xacro
+
+def evaluate_xacro(context, *args, **kwargs):
+    name = LaunchConfiguration('name').perform(context)
+    topics_ns = LaunchConfiguration('topics_ns').perform(context)
+    add_plug = LaunchConfiguration('add_plug').perform(context)
+    publish_pointcloud = LaunchConfiguration('publish_pointcloud').perform(context)
+    use_nominal_extrinsics = LaunchConfiguration('use_nominal_extrinsics').perform(context)
+    use_mesh = LaunchConfiguration('use_mesh').perform(context)
+    model = LaunchConfiguration('model').perform(context)
+  
+    robot_state_publisher_node = Node(
+      package='robot_state_publisher',
+      executable='robot_state_publisher',
+      name='robot_state_publisher',
+      #namespace=LaunchConfiguration('topics_ns'),
+      output='screen',
+      parameters=[{
+        'use_sim_time': False,
+        'robot_description': xacro.process_file(model, mappings={'name': name,
+                                                                 'topics_ns': topics_ns,
+                                                                 'add_plug': add_plug,
+                                                                 'publish_pointcloud': publish_pointcloud,
+                                                                 'use_nominal_extrinsics': use_nominal_extrinsics,
+                                                                 'use_mesh': use_mesh}).toxml()
+      }])
+
+    return [robot_state_publisher_node]
 
 def generate_launch_description():
 
-    xacro_path = os.path.join(get_package_share_directory('realsense2_description'), 'urdf', 'test_l515_camera.urdf.xacro')
-
-    urdf = to_urdf(xacro_path, {'use_nominal_extrinsics': 'true', 'add_plug': 'true'})
+    xacro_path = os.path.join(get_package_share_directory('realsense2_description'), 'urdf', 'cam_l515.urdf.xacro')
 
     # Arguments
     name_arg = DeclareLaunchArgument(
@@ -36,30 +63,69 @@ def generate_launch_description():
         default_value='true',
         description='Flag to publish point cloud'
     )
+    use_nominal_extrinsics_arg = DeclareLaunchArgument(
+        'use_nominal_extrinsics',
+        default_value='true',
+        description='Flag to create camera'
+    )
+    use_mesh_arg = DeclareLaunchArgument(
+        'use_mesh',
+        default_value='true',
+        description='Flag to use meshes files'
+    )
     model_arg = DeclareLaunchArgument(
         'model',
         default_value= xacro_path,
         description='Path to URDF xacro file'
     )
 
+
+    #name_var = str(LaunchConfiguration('name'))
+    #name_str = LaunchContext.perform_substitution(LaunchConfiguration('name'))
+    #topic_var = str(LaunchConfiguration('topics_ns'))
+    #topic_str = LaunchContext.perform_substitution(LaunchConfiguration('topics_ns'))
+    name_str = ['a',LaunchConfiguration('name')]
+    topic_str = ['a',LaunchConfiguration('topics_ns')]
+
+    #urdf = to_urdf(xacro_path, {'name':''.join(name_str), 'topics_ns':''.join(topic_str), 'use_nominal_extrinsics': 'true', 'add_plug': 'true'})
     # Robot Description
-    robot_description_cmd = f"xacro --inorder '{xacro_path}' use_nominal_extrinsics:=true publish_pointcloud:=true add_plug:=false name:=camera topics_ns:=camera"
+    #robot_description_cmd = f"xacro --inorder '{xacro_path}' use_nominal_extrinsics:=true publish_pointcloud:=true add_plug:=false name:=c1 topics_ns:=c1"
     
-    robot_description = DeclareLaunchArgument(
+    """robot_description = DeclareLaunchArgument(
         'robot_description',
         default_value=robot_description_cmd,
         description='Robot description'
-    )
+    )"""
 
-    # Robot State Publisher
-    robot_state_publisher_node = Node(
+    """
+    doc = xacro.process_file(xacro_path, 
+        mappings={name, 
+                    topics_ns, 
+                    publish_pointcloud, 
+                    use_nominal_extrinsics, 
+                    add_plug,
+                    use_mesh
+                    })
+    robot_desc = doc.toprettyxml(indent='  ')
+    params = {'robot_description': robot_desc}
+    robot_state_publisher_node = Node(package='robot_state_publisher',
+                                node_executable='robot_state_publisher_node',
+                                namespace=LaunchConfiguration('topics_ns'),
+                                output='screen',
+                                parameters=[params])
+    """
+
+        # Robot State Publisher
+    """robot_state_publisher_node = Node(
         name='model_node',
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        namespace='',
+        namespace=LaunchConfiguration('topics_ns'),
         output='screen',
-        arguments=[urdf]
-    )
+        #arguments=[urdf]
+        parameters=[{
+                "robot_description": LaunchConfiguration('robot_desc')}]
+    )"""
 
     # Gazebo Launch
     
@@ -102,12 +168,15 @@ def generate_launch_description():
         topics_ns_arg,
         add_plug_arg,
         publish_pointcloud_arg,
+        use_nominal_extrinsics_arg,
+        use_mesh_arg,
         model_arg,
-        robot_description,
-        robot_state_publisher_node,
+        #create_robot_description_arg,
+        #robot_description,
+        #robot_state_publisher_node,
         gazebo_launch,
         gui_arg,
         use_gui_param,
-        rviz_node
+        rviz_node,
+        OpaqueFunction(function=evaluate_xacro)
     ])
-
