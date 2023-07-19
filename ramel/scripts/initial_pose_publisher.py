c3722ae5706554db8eb5d84ad936e2b12a0418a7
antepigu <antepigu@espol.edu.ec>
2023-07-19 14:57:27 -0500

    [OK]Initial pose publisher for robots and added aruco marker on r1

--- ramel/scripts/initial_pose_publisher.py
+++ ramel/scripts/initial_pose_publisher.py
@@ -1,10 +1,10 @@
 import rclpy
-import rospy
 from rclpy.node import Node
 
 from std_msgs.msg import String
 from aruco_msgs.msg import Poses
 from geometry_msgs.msg import PoseWithCovarianceStamped
+import math 
 
 
 class InitialPosePublisher(Node):
@@ -13,7 +13,8 @@ class InitialPosePublisher(Node):
         super().__init__('initialpose_publisher')
 
         self.num_robots = num_robots
-        robot_topics = ['r' + str(i + 1) for i in range(self.num_robots)]
+        self.dataloaded = False
+        self.robot_topics = ['r' + str(i + 1) for i in range(self.num_robots)]
 
         self.subscription = self.create_subscription(
             Poses,
@@ -29,14 +30,28 @@ class InitialPosePublisher(Node):
         #        lambda msg, robot_topic=robot_topic: self.marker_callback(msg, robot_topic),
         #        10
         #    )
+        
+        if(self.dataloaded):
+            print('11111111111')
+            for robot_topic in robot_topics:
+                
+                print('222222222')
+                self.publisher_ = self.create_publisher(
+                    PoseWithCovarianceStamped,
+                    '/'+robot_topic+'/initialpose',
+                    10
+                )
+                msg = PoseWithCovarianceStamped()
+                for id in self.markers_id:
+                    if((int(robot_topic[-1])-1) == id):
+                        msg.pose = self.poses_array[0]
+                        break
+                msg.header.stamp = self.get_clock().now().to_msg()
+                msg.header.frame_id = "origin_frame"
+                print(msg)
+                #msg.data = 'Hello World: %d' % self.i
+                self.publisher_.publish(msg)
 
-        for robot_topic in robot_topics:
-            self.publisher_ = self.create_publisher(
-                PoseWithCovarianceStamped,
-                '/'+robot_topic+'/initialpose',
-                lambda msg, robot_topic=robot_topic: self.initialPoses_callback(msg, robot_topic),
-                10
-            )
 
         #self.publisher_ = self.create_publisher(PoseWithCovarianceStamped, '/'+robot_topic+'/initialpose', 10)
         #timer_period = 0.5  # seconds
@@ -46,16 +61,46 @@ class InitialPosePublisher(Node):
     def getPoses_callback(self, msg):
         self.markers_id = msg.marker_ids
         self.poses_array = msg.poses
+        self.dataloaded = True
+
+        print('11111111111')
+        for robot_topic in self.robot_topics:
+            
+            print('222222222')
+            self.publisher_ = self.create_publisher(
+                PoseWithCovarianceStamped,
+                '/'+robot_topic+'/initialpose',
+                10
+            )
+            msg = PoseWithCovarianceStamped()
+            for id in self.markers_id:
+                if((int(robot_topic[-1])-1) == id):
+                    print(self.poses_array[0])
+                    marker_index = self.markers_id.index(id)
+                    msg.pose.pose.position.x = self.poses_array[marker_index].position.x
+                    msg.pose.pose.position.y = self.poses_array[marker_index].position.y
+                    msg.pose.pose.position.z = 0.0
+                    msg.pose.pose.orientation = self.poses_array[marker_index].orientation
+                    msg.pose.covariance[6*0+0] = 0.5 * 0.5
+                    msg.pose.covariance[6*1+1] = 0.5 * 0.5
+                    msg.pose.covariance[6*5+5] = math.pi/12.0 * math.pi/12.0
+                    break
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.header.frame_id = "map"
+            print(msg)
+            #msg.data = 'Hello World: %d' % self.i
+            self.publisher_.publish(msg)
+
         print()
 
 
     def initialPoses_callback(self, msg, robot_topic):
-
+        msg = PoseWithCovarianceStamped()
         for id in self.markers_id:
             if((int(robot_topic[-1])-1) == id):
-                msg.pose = self.poses_array[0]
+                msg.pose.pose = self.poses_array[0]
                 break
-        msg.header.stamp = rospy.get_rostime()
+        msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = "map"
 
         #msg.data = 'Hello World: %d' % self.i
