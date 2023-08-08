6247201bb84ad026fe8f71f93b43e0c4f9acab24
DiegoRonquillo4 <diromano@espol.edu.ec>
2023-08-08 09:08:39 -0500

    Desktop executable file

---
+++ ramel/framework.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the directory of the current script
+script_dir="$(dirname "$(readlink -f "$0")")"
+
+# Navigate to the parent directory
+cd "$script_dir/../../.."
+
+
+# Source the setup script
+source install/setup.bash
+
+# Navigate to the directory
+cd "$script_dir"
+
+# Run the Python script
+python3 GUI.py
+
